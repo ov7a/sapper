cecf6d7f7f285ee6eb6cea51cd14c41d09b8247b
ov7a <ov7a@yandex.ru>
2024-06-30 14:25:54 +0400

    refactoring

--- src/sapper.nim
+++ src/sapper.nim
@@ -1,50 +1,57 @@
 import cart/wasm4
 import std/random
 
-const WIDTH = 16
-const HEIGHT = WIDTH
-const CELL_WIDTH = SCREEN_SIZE div WIDTH
-const CELL_HEIGHT = SCREEN_SIZE div HEIGHT
-const MINES = 40
-
 type GameState = enum START, IN_PROGRESS, OVER, VICTORY
 var gameState: GameState = START
 
 ### Field
 
+type
+  CellValue = uint8
+  Coord = int # good luck changing this to int8
+  Cell = (Coord, Coord)
+
+const 
+  WIDTH: Coord = 16
+  HEIGHT: Coord = WIDTH
+  FIELD_SIZE: int = WIDTH * HEIGHT
+  MINES = 40
+
 # for a cell: 
 # open: 0-8 - number of surrounding mines
 # closed: 10 - empty, 11 - mine
-const HIDDEN_MINE = 11
-const UNKNOWN = 10
-var field: array[WIDTH, array[HEIGHT, uint8]]
-var closedCells = WIDTH*HEIGHT
+const 
+  HIDDEN_MINE = 11
+  UNKNOWN = 10
 
-iterator neighbors(x: int, y: int): (int, int) =
-  if (x > 0): yield (x - 1, y)
-  if (x < WIDTH - 1): yield (x + 1, y)
-  if (y > 0): yield (x, y - 1)
-  if (y < HEIGHT - 1): yield (x, y + 1)
+var 
+  field: array[WIDTH, array[HEIGHT, CellValue]]
+  closedCells = FIELD_SIZE
 
 proc isSolvable: bool =
   static: assert WIDTH*HEIGHT <= 1 shl sizeof(uint8)
   var visited: set[uint8]
+  proc pos(p: Cell): uint8 = uint8(p[0] * WIDTH + p[1])
 
-  proc pos(x: int, y: int): uint8 = uint8(x * WIDTH + y)
+  iterator neighbors(p: Cell): Cell =
+    let (x, y) = p
+    if (x > 0): yield (x - 1, y)
+    if (x < WIDTH - 1): yield (x + 1, y)
+    if (y > 0): yield (x, y - 1)
+    if (y < HEIGHT - 1): yield (x, y + 1)
 
-  var stack: seq[(int, int)] = @[(WIDTH div 2, HEIGHT div 2)]
+  var stack: seq[Cell] = @[(WIDTH div 2, HEIGHT div 2)]
 
   while stack.len > 0:
-    let (x, y) = stack.pop()
-    visited.incl(pos(x, y))
-    trace("visited " & $visited.len)
-    for (nx, ny) in neighbors(x, y):
-      if (not (pos(nx, ny) in visited)) and field[nx][ny] != HIDDEN_MINE:
-        stack.add((nx, ny))
-  trace("git " & $(visited.len) & "  " & $(WIDTH*HEIGHT - MINES ))
-  visited.len == WIDTH*HEIGHT - MINES       
+    let current = stack.pop()
+    visited.incl(current.pos())
+    for neighbor in neighbors(current):
+      if (not (neighbor.pos() in visited)) and field[neighbor[0]][neighbor[1]] != HIDDEN_MINE:
+        stack.add(neighbor)
 
-proc generateField(sapperX: int, sapperY: int) = 
+  visited.len == FIELD_SIZE - MINES     
+
+proc generateField(startX: Coord, startY: Coord) = 
   while true:
     for x in 0..<WIDTH:
       for y in 0..<HEIGHT:
@@ -54,14 +61,14 @@ proc generateField(sapperX: int, sapperY: int) =
     while minesLeft > 0:
       let mineX = rand(WIDTH - 1)
       let mineY = rand(HEIGHT - 1)
-      if field[mineX][mineY] == UNKNOWN and (mineX != sapperX or mineY != sapperY):
+      if field[mineX][mineY] == UNKNOWN and (mineX != startX or mineY != startY):
         field[mineX][mineY] = HIDDEN_MINE
         minesLeft -= 1     
 
     if (isSolvable()):
       break
 
-proc surroundingMines(x: int, y: int): uint8 =
+proc surroundingMines(x: Coord, y: Coord): uint8 =
   uint8(x > 0 and y > 0 and field[x-1][y-1] == HIDDEN_MINE) + 
   uint8(x > 0 and field[x-1][y] == HIDDEN_MINE) + 
   uint8(x > 0 and y < HEIGHT - 1 and field[x-1][y+1] == HIDDEN_MINE) + 
@@ -71,7 +78,7 @@ proc surroundingMines(x: int, y: int): uint8 =
   uint8(x < WIDTH-1 and field[x+1][y] == HIDDEN_MINE) + 
   uint8(x < WIDTH-1 and y < HEIGHT-1 and field[x+1][y+1] == HIDDEN_MINE)
 
-proc maybeOpenCell(x: int, y: int) =
+proc maybeOpenCell(x: Coord, y: Coord) =
   case field[x][y]
     of HIDDEN_MINE:
       gameState = OVER
@@ -85,10 +92,11 @@ proc maybeOpenCell(x: int, y: int) =
 
 ### Sapper
 
-var sapperX: int 
-var sapperY: int
+var 
+  sapperX: Coord
+  sapperY: Coord
 
-proc moveSapper(x: int, y: int) =
+proc moveSapper(x: Coord, y: Coord) =
   sapperX = x
   sapperY = y
   maybeOpenCell(x,y)
@@ -106,25 +114,29 @@ proc mayBeMoveSapper(keyPressed: uint8) =
 ### Game state
 
 proc restart =
-  closedCells = WIDTH*HEIGHT
+  closedCells = FIELD_SIZE
   let 
-    startX = rand(WIDTH-1)
-    startY = rand(HEIGHT-1)
+    startX = Coord(rand(WIDTH-1))
+    startY = Coord(rand(HEIGHT-1))
   generateField(startX, startY)
   moveSapper(startX, startY)
   gameState = IN_PROGRESS
 
 ### Drawing
 
-proc drawSapper(x: int, y: int) = 
+const 
+  CELL_WIDTH = SCREEN_SIZE div WIDTH
+  CELL_HEIGHT = SCREEN_SIZE div HEIGHT
+
+proc drawSapper(x: Coord, y: Coord) = 
   DRAW_COLORS[] = 0x4
   oval(x * CELL_WIDTH + 3, y * CELL_HEIGHT + 3, CELL_WIDTH - 6, CELL_HEIGHT - 6)
 
-proc drawUnknownCell(x: int, y: int) =
+proc drawUnknownCell(x: Coord, y: Coord) =
   DRAW_COLORS[] = 0x23
   rect(x * CELL_WIDTH, y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT)
 
-proc drawCell(x: int, y: int) = 
+proc drawCell(x: Coord, y: Coord) = 
   case field[x][y]
     of UNKNOWN:
       drawUnknownCell(x, y)
@@ -141,12 +153,12 @@ proc drawCell(x: int, y: int) =
       discard  
 
 proc drawField =
-  for x in 0..WIDTH:
-    for y in 0..HEIGHT:
+  for x in 0..<WIDTH:
+    for y in 0..<HEIGHT:
       drawCell(x,y)
 
-proc printCenter(text: string, y: int) =
-  text(text, (SCREEN_SIZE - text.len*FONT_SIZE) div 2, y)
+proc printCenter(text: string, y: Coord) =
+  text(text, (SCREEN_SIZE - text.len * FONT_SIZE) div 2, y)
 
 proc drawStartScreen =
   DRAW_COLORS[] = 0x2
